1c344e1253964c213380acb6a0e3ffad1a1d3f35
Jhon Sidney <jhon.sidney@gmail.com>
2020-09-21 23:10:40 -0300

    com erros;

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ struct Lutador{
     int id;
     int time;
     int valorDano;
-    int pontos;
+    int pontosVida;
     int iniciativa;
 };
 
@@ -18,19 +18,20 @@ struct Iniciativa{
 };
 
 
+
+struct Iniciativa* removerLutador(struct Iniciativa *head, int id);
+
+
+
+
 int main(){
-    int quantidadeTime1, quantidadeTime2;
     int opcao = menuPrincipal();
     while(opcao!=0){
         switch(opcao){
             case 1:
                 system("cls");
-                printf("informe o numero de lutadores da equipe 1: ");
-                scanf("%d",&quantidadeTime1);
-                printf("informe o numero de lutadores da equipe 2: ");
-                scanf("%d",&quantidadeTime2);
 
-                iniciarJogo(quantidadeTime1, quantidadeTime2);
+                iniciarJogo();
 
                 break;
             case 2:
@@ -52,13 +53,66 @@ int main(){
 
 
 
-void iniciarJogo(int qtdLutadoresTime, int qtdlutadoresTime2){
-    struct Lutador time1[qtdlutadoresTime];
-    struct Lutador time2[qtdLutadoresTime2];
+void iniciarJogo(){
+    struct Iniciativa *iniciativa = malloc(sizeof (struct Iniciativa));
+    iniciativa->next = NULL;
+    iniciativa->l = NULL;
+    organizacaoTime(iniciativa);
 
-    organizacaoTime(time1,qtdLutadoresTime,1);
-    organizacaoTime(time2,qtdLutadoresTime2,2);
 
+}
+
+void organizacaoTime(struct Iniciativa *iniciativa){
+    int opcao;
+    int id;
+    struct Iniciativa *last = iniciativa;
+
+    while(1){
+        system("cls");
+        printf("1. insercao\n");
+        printf("2. status \n");
+        printf("3. fuga\n");
+        printf("4. voltar\n");
+
+        scanf("%d",&opcao);
+
+        switch(opcao){
+        case 1:
+
+             insercaoLutador(last);
+             if(last->next != NULL){
+                last = last-> next;
+             }
+             printf("Adicionado !\n");
+             system("pause");
+             break;
+        case 2:
+             printf("status");
+             statusLutadores(iniciativa);
+             system("pause");
+             break;
+        case 3:
+
+             printf("informe o id:");
+             scanf("%d",&id);
+
+             struct Iniciativa *aux = removerLutador(iniciativa,id);
+             if(aux != NULL){
+                    last = aux;
+
+             }
+             system("pause");
+             break;
+        case 4:
+            printf("voce saiu\n");
+            break;
+            break;
+        default:
+            printf("opcao invalida \n");
+
+        }
+
+    }
 
 
 
@@ -68,16 +122,88 @@ void iniciarJogo(int qtdLutadoresTime, int qtdlutadoresTime2){
 
 }
 
-void organizacaoTime(struct Lutador *ptime, int qtdLutadores,int time){
+
+
+void insercaoLutador(struct Iniciativa *iniciativa){
     static int k=0;
-    for(int i=0; i< qtdLutadores;i++){
-            ptime[i].id = k++;
-            ptime[i].time = time;
-            ptime[i].valorDano=20;
-            ptime[i].pontosVida= 20;
-            ptime[i].iniciativa=20;
+    struct Iniciativa *novoLutador = malloc(sizeof(struct Iniciativa));
+
+    novoLutador->l = malloc (sizeof(struct Lutador));
+    novoLutador->next = NULL;
+
+    novoLutador->l->id = k++;
+    novoLutador->l->time = 1;
+    novoLutador->l->valorDano = 20;
+    novoLutador->l->pontosVida = 20;
+    novoLutador->l->iniciativa = 20;
+
+    if(iniciativa->l != NULL){
+        iniciativa->next = novoLutador;
+
+    }else{
+        iniciativa->l = novoLutador->l;
+        free(novoLutador); //liberando espaço
     }
 }
+
+
+
+
+void statusLutadores(struct Iniciativa *head){
+    int i =1;
+    printf("========================\n");
+    while(head != NULL){
+        printf("%d\n",i);
+        printf("iniciativa: %d\n",head->l->iniciativa);
+        printf("id: %d\n",head->l->id);
+        printf("pontos de vida: %d\n",head->l->pontosVida);
+        printf("Time: %d\n",head->l->time);
+        printf("valor Dano: %d\n",head->l->valorDano);
+        printf("========================\n");
+
+        head = head->next;
+        i++;
+    }
+
+
+
+
+}
+
+
+
+
+
+struct Iniciativa* removerLutador(struct Iniciativa *head, int id){
+    struct Iniciativa *proximo;
+
+    if(head != NULL && head->l->id == id){
+        free(head->l);
+        proximo = head->next;
+        head->l = proximo->l;
+        head->next = proximo->next;
+        free(proximo);
+        return NULL;
+    }
+
+
+    while(head != NULL){
+        proximo = head->next;
+        if(proximo != NULL && proximo->l->id == id){
+            head->next = proximo->next;
+            free(proximo);
+            break;
+        }
+
+        head = head->next;
+    }
+    if (head->next != NULL){
+        return NULL;
+    }
+    return head;
+}
+
+
 
 
 int  menuPrincipal(){
